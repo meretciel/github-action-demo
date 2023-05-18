23994dcaeaaf89b89b1168b79aa57370766d6013
Ruikun Hong <ruikun@Ruikun-MacbookPro.local>
2023-05-17 22:03:52 -0400

    Simple code plus workflow yml file.

---
+++ GithubActionDemo/test/subfolder/test_TestInSubfolder.py
@@ -0,0 +1,9 @@
+
+import unittest
+
+class TestInSubfolder(unittest.TestCase):
+    def test_A(self):
+        print("running TestInSubfolder.test_A")
+
+    def test_B(self):
+        print("running TestInSubfolder.test_B")
---
+++ GithubActionDemo/test/test_simpleTest.py
@@ -0,0 +1,9 @@
+
+import unittest
+
+class TestCase(unittest.TestCase):
+    def test_1(self):
+        print("running TestCase.test_1")
+
+    def test_2(self):
+        print("running TestCase.test_2")
